a9f18f24da143c723e671f6ef033fdc18d76d4f3
Shukri Adams <shukri.adams@gmail.com>
2022-05-27 10:27:08 +0200

    added build action

---
+++ build.sh
@@ -0,0 +1,25 @@
+set -e
+
+DOCKERPUSH=0
+while [ -n "$1" ]; do 
+    case "$1" in
+    --dockerpush) 
+        DOCKERPUSH=1 ;; 
+    esac 
+    shift
+done
+
+docker build -t shukriadams/docker-p4client .
+echo "container built"
+
+if [ $DOCKERPUSH -eq 1 ]; then
+    echo "starting docker push"
+    TAG=$(git describe --tags --abbrev=0) 
+    echo "Tag ${TAG} detected"
+
+    docker tag shukriadams/duplicity:latest shukriadams/docker-p4client:"${TAG}"
+    docker login -u $DOCKER_USER -p $DOCKER_PASS 
+    docker push shukriadams/docker-p4client:$TAG
+fi
+
+echo "build complete"
